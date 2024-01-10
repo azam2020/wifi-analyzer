1ecb260f37453c4b90b6cfe86253a92bc6a8d952
azam2020 <azam8933033@gmail.com>
2024-01-10 15:18:14 +0530

    hover

--- updated_wifi.py
+++ updated_wifi.py
@@ -186,4 +186,4 @@ def speed_test():
 	return render_template('speed.html',link=link)
 
 if __name__=='__main__':
-	app.run(host='0.0.0.0',port=5000,debug=True)
+	app.run(host='0.0.0.0',port=5001,debug=True)
