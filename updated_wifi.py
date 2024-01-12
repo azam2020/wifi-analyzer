eb04cff7e0fa2b65560527bcd3bb04947b2c583d
Mohammad Azam <68936941+azam2020@users.noreply.github.com>
2024-01-13 02:30:45 +0530

    Update updated_wifi.py

--- updated_wifi.py
+++ updated_wifi.py
@@ -235,4 +235,4 @@ def documentation():
 	return render_template('documentation.html')
 
 if __name__=='__main__':
-	app.run(host='0.0.0.0',port=5001,debug=True)
+	app.run(host='0.0.0.0',debug=True)
